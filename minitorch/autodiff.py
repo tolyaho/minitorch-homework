38213624ea772801b2b100dbc8953d1260506ce6
tolyaho <tolyaho@users.noreply.github.com>
2026-09-12 23:06:32 +0300

    implement task 1.1 central difference

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -25,8 +25,9 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
 
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    plus = [v + epsilon if i == arg else v for i, v in enumerate(vals)]
+    minus = [v - epsilon if i == arg else v for i, v in enumerate(vals)]
+    return (f(*plus) - f(*minus)) / (2 * epsilon)
 
 
 variable_count = 1
